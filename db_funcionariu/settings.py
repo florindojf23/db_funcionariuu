4326970cbabbfd054ca0af970aac5654c280b4a1
Florindo Junior Fernandes <54613606+florindojf23@users.noreply.github.com>
2024-08-08 18:00:21 +0900

    hadia settings

--- db_funcionariu/settings.py
+++ db_funcionariu/settings.py
@@ -145,9 +145,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL =  '/images/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static_files')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR,'images')
 
